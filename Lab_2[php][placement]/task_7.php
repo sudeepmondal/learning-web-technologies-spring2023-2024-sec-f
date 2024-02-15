9d2baaaf5869e0a1573dc82cf8d6b6b075d45d82
Sudeep Mondal Deep <157618228+sudeepmondal@users.noreply.github.com>
2024-02-15 23:46:32 +0600

    Update task_7.php

--- Lab_2[php][placement]/task_7.php
+++ Lab_2[php][placement]/task_7.php
@@ -1,56 +1,37 @@
-<html>
-<head>
-    <title>Task_7</title>
-</head>
-<body>
-    <table>
-        <tr>
-            <th>Stars</th>
-            <th>Numbers</th>
-            <th>Letters</th>
-        </tr>
-        <tr>
-            <td>
-                <?php
-               
-                for ($i = 0; $i < 3; $i++) 
-                {
-                    for ($j = 0; $j <= $i; $j++) 
-                    {
-                        print("*");
-                    }
-                    print("<br>");
-                }
-                ?>
-            </td>
-            <td>
-                <?php
-           
-                for ($i = 3; $i >= 1; $i--) 
-                {
-                    for ($j = 1; $j <= $i; $j++) 
-                    {
-                        print($j . " ");
-                    }
-                    print("<br>");
-                }
-                ?>
-            </td>
-            <td>
-                <?php
-               
-                $word = 'A';
-                for ($i = 0; $i < 3; $i++)
-                {
-                    for ($j = 0; $j <= $i; $j++) 
-                    {
-                        print($word++ . " ");
-                    }
-                    print("<br>");
-                }
-                ?>
-            </td>
-        </tr>
-    </table>
-</body>
-</html>
+<?php
+echo "<table cellpadding='10' cellspacing='0' border='1' height='50px' width='300px'>" ; 
+echo "<td>" ;
+for ($i = 1; $i <= 3; $i++) 
+{
+    for ($j = 1; $j <= $i; $j++) 
+    {
+        echo  "* " ;
+    }
+    echo "<br>" ;
+}
+
+echo "</td>" ;
+echo "<td>" ;
+for ($i =1; $i <= 3; $i++) 
+{
+    for ($j = 1; $j <= (4 - $i); $j++) 
+    {
+        echo $j. " " ;
+    }
+    echo "<br>" ;
+}
+echo "</td>" ;
+echo "<td>" ;
+$char = 'A' ;
+for ($i = 1; $i <= 3; $i++) 
+{
+    for ($j = 1; $j <= $i; $j++) 
+    {
+        echo $char++ . " " ;
+    }
+    echo "<br>" ;
+}
+echo "</td>" ;
+echo "</tr>" ;
+echo "</table>" ;
+?>
