4683c592073ae8a7b0a0db85c6549ecdb8c9e414
Sudeep Mondal Deep <157618228+sudeepmondal@users.noreply.github.com>
2024-03-08 09:43:50 +0600

    Update task_C.php

--- Lab_5.2[PHP][Session Tracking]/task_C.php
+++ Lab_5.2[PHP][Session Tracking]/task_C.php
@@ -1,39 +1,45 @@
 <html>
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Login</title>
+    <title>Login</title>
+    <style>
+        fieldset 
+        {
+            width: 400px; 
+        }
+        hr 
+        {
+            width: 400px; 
+        }
+    </style>
 </head>
 <body>
-<table style="width: 100%;" border="1">
-<tr>
-<td style="text-align: left;">X Company</td>
-<td align="right">
-<a href="home.php">Home</a> |
-<a href="login.php">Login</a> |
-<a href="registration.php">Registration</a>
-</td>
-</tr>
-<tr>
-<td colspan="2">
-<h1>Login Page</h1>
-<fieldset>
-<legend>Login Form</legend>
-<label for="username">Username:</label><br>
-<input type="text" id="username" name="username"><br>
-<label for="password">Password:</label><br>
-<input type="password" id="password" name="password"><br><br>
-<button type="submit">Login</button>
-</fieldset>
-</td>
-</tr>
-<tr>
-<td colspan="2" style="text-align: center;">
-<a href="forgot_password.html">Forgot Password?</a><br>
-<br>
-Copyright &copy; 2017
-</td>
-</tr>
-</table>
+    <table style="width: 720px;" border="1">
+        <tr>
+            <td>
+                <b>&emsp;X Company</b> &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;   
+                <a href="task_A.php">Home</a> |
+                <a href="task_B.php">Registration</a> |
+                <a href="task_C.php">Login</a>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <fieldset>
+                    <legend>LOGIN</legend>
+                    User Name:<input type="text" id="username" name="username"> <br>
+                    Password :<input type="password" id="password" name="password">
+                    <hr>
+                    <input type="checkbox" id="remember_me" name="remember_me">Remember me
+                    <br> <br>
+                    <button type="submit">Submit</button> <a href="forgot_password.html">Forgot Password?</a><br>   
+                </fieldset>
+            </td>
+        </tr>
+        <tr>
+            <td style="text-align: center;">
+                 Copyright &copy; 2017
+            </td>
+        </tr>
+    </table>
 </body>
 </html>
