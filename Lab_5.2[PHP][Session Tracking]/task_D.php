d4d169b4ecd82c4d6250ee61dcff9c5e63cb9ab3
Sudeep Mondal Deep <157618228+sudeepmondal@users.noreply.github.com>
2024-03-08 09:39:57 +0600

    Update task_D.php

---
+++ Lab_5.2[PHP][Session Tracking]/task_D.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+    <title>Forgot Password</title>
+    <style>
+        fieldset 
+        {
+            width: 400px; 
+            margin: 0 auto; 
+            text-align: left; 
+        }
+        hr 
+        {
+            width: 100%; 
+            margin: 10px 0; 
+        }
+    </style>
+</head>
+<body>
+    <table style="width: 720px;" border="1">
+        <tr>
+            <td>
+                &emsp;<b> X Company &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;   
+                <a href="task_A.php">Home</a> |
+                <a href="task_B.php">Registration</a> |
+                <a href="task_C.php">Login</a>
+            </td>
+        </tr>
+        <tr>
+            <td style="text-align: center;">
+                <fieldset>
+                    <legend>Forgot Password</legend>
+                    Email:<input type="email" id="email" name="email"><br>
+                    <hr>
+                    <button type="submit">Submit</button>
+                </fieldset>
+            </td>
+        </tr>
+        <tr>
+            <td style="text-align: center;">
+                 Copyright &copy; 2024
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
