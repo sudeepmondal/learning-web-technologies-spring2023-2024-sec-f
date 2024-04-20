3b284c95b10fd5bac0d7796db01018231100a12c
Sudeep Mondal Deep <157618228+sudeepmondal@users.noreply.github.com>
2024-04-20 17:14:20 +0600

    Update task6.php

--- FinalTask2/task6.php
+++ FinalTask2/task6.php
@@ -1,33 +1,27 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Blood Group</title>
+    <title>Date of Birth</title>
 </head>
 <body>
-    <label for="Blood">Blood Group</label>
-    <select id="Blood">
-        <option value="option1" selected>Select</option>
-        <option value="option2">B+</option>
-        <option value="option3">AB-</option>
-		<option value="option4">A+</option>
-        <option value="option5">O+</option>
-        <option value="option6">AB+</option>
-        <option value="option7">B-</option>
-        <option value="option8">O-</option>
-    </select>
-    <hr>
-    <button onclick="checkBloodGroup()">Submit</button>
-    <h4 id="message"></h4>
-    <script>
-        function checkBloodGroup() {
-            let selectedOption = document.getElementById("Blood").value;
+    <fieldset>
+        <legend>Date of Birth</legend>
+        <input type="text" id="dob" placeholder="DD/MM/YYYY">
+        <button onclick="checkDOB()">Submit</button>
+        <h4 id="message"></h4>
+    </fieldset>
 
-            if (selectedOption === "option1") {
-                document.getElementById('message').innerHTML = "Please select a Blood Group";
-                
+    <script>
+        function checkDOB() {
+            let dob = document.getElementById("dob").value.trim();
+            let datePattern = /^(0?[1-9]|[12][0-9]|3[01])\/(0?[1-9]|1[012])\/(19\d\d|20(0\d|1[0-6]))$/;
+
+            if (dob === "") {
+                document.getElementById('message').innerHTML = "Date of birth cannot be empty.";
+            } else if (!datePattern.test(dob)) {
+                document.getElementById('message').innerHTML = "Invalid date of birth format.";
             } else {
-                document.getElementById('message').innerHTML = "Form submitted successfully!";
-                
+                document.getElementById('message').innerHTML = "Form submitted successfully!";       
             }
         }
     </script>
